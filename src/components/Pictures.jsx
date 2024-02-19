fb2a97c1b00229cca7f72062cd884c13dd2ca57e
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-02-18 21:57:05 -0600

    Updated pictures

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -32,34 +32,7 @@ const Pictures = () => {
             <img
               alt="gallery"
               class="block h-full w-full rounded-lg object-cover object-center"
-              src={pic3}
-            />
-          </div>
-        </div>
-        <div class="flex w-1/3 flex-wrap">
-          <div class="w-full p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src={pic4}
-            />
-          </div>
-        </div>
-        <div class="flex w-1/3 flex-wrap">
-          <div class="w-full p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
               src={pic5}
-            />
-          </div>
-        </div>
-        <div class="flex w-1/3 flex-wrap">
-          <div class="w-full p-1 md:p-2">
-            <img
-              alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
-              src={pic1}
             />
           </div>
         </div>
