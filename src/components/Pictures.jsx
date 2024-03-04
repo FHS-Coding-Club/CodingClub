d331daab3e90fd8d05debf0d0de1db841158934e
Anant-raj2 <122118125+Rishi-S20@users.noreply.github.com>
2024-03-04 15:19:08 -0600

    Made responsive

--- src/components/Pictures.jsx
+++ src/components/Pictures.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import pic1 from "../assets/codingclub1.jpg";
 import pic2 from "../assets/codingclub2.jpg";
-import pic3 from "../assets/codingclub3.jpg";
-import pic4 from "../assets/codingclub4.jpg";
 import pic5 from "../assets/codingclub5.jpg";
 
 const Pictures = () => {
   return (
-    <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+    <div className="container mx-auto px-5 py-2 lg:px-10 lg:pt-12">
       <div className="flex flex-wrap justify-center -m-1 md:-m-2">
         {[pic1, pic2, pic5].map((pic, index) => (
           <div
             key={index}
-            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-1 md:p-2"
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 md:p-2"
           >
             <img
               alt="gallery"
